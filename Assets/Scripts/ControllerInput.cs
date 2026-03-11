35328071d790956fba1b99071ed06af95ed18e1b
Ryan Grandin <rmgrandin8@gmail.com>
2026-03-11 11:55:10 -0400

    Completed challenge 1 and started challenge 2.

--- Assets/Scripts/ControllerInput.cs
+++ Assets/Scripts/ControllerInput.cs
@@ -6,6 +6,7 @@ public class ControllerInput : MonoBehaviour
     public float speed = 5;
     public Vector2 movement;
     public AudioSource SFX;
+    public Vector2 look;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,9 +18,9 @@ public class ControllerInput : MonoBehaviour
     void Update()
     {
         // use with stick
-        //transform.position += (Vector3)movement * speed * Time.deltaTime;
+        transform.position += (Vector3)movement * speed * Time.deltaTime;
         // use with pointer
-        transform.position = movement;
+        //transform.position = movement;
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -40,5 +41,15 @@ public class ControllerInput : MonoBehaviour
     public void OnPoint(InputAction.CallbackContext context)
     {
         movement = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>());
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        look = context.ReadValue<Vector2>();
+
+        if (look.x == transform.position.x)
+        {
+            //transform.right 
+        }
     }
 }
