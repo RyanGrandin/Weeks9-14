61d9bac7ada2c38c234671ba2040f4cef7b802a2
Ryan Grandin <rmgrandin8@gmail.com>
2026-03-17 21:07:24 -0400

    Required reaading 10.1. Made a new scene. Added a tree. Gave the tree a new script. Added a canvas and button. When button is pressed, a coroutine within the script is started and the tree grows.

---
+++ Assets/Scripts/Grower.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
+public class Grower : MonoBehaviour
+{
+    public Transform treeTransform;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        treeTransform.localScale = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartTreeGrowing()
+    {
+        StartCoroutine(GrowTree());
+    }
+
+    IEnumerator GrowTree()
+    {
+        float t = 0;
+
+        while (t < 1)
+        {
+            t += Time.deltaTime;
+            treeTransform.localScale = Vector2.one * t;
+            yield return null;
+        }
+    }
+}
