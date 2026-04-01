d63a20bc4595dac28020eb858b385adf44b00e99
Ryan Grandin <rmgrandin8@gmail.com>
2026-04-01 11:37:03 -0400

    - Added a knight that can move. - Added tilemap. - Added cinemachine camera which follows the knight. - When not moving, idle animation plays. - When moving, run animation plays. - On run animation event (footsteps), screen shakes and, if they are bricks, the tile at knight position changes to cracked bricks.

--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -1,8 +1,19 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Unity.Cinemachine;
+using UnityEngine.Tilemaps;
+using UnityEngine.Rendering;
 
 public class Knight : MonoBehaviour
 {
-    public AudioSource SFX;
+    //public AudioSource SFX;
+    Vector2 movement;
+    public float speed = 3;
+    public CinemachineImpulseSource impulseSource;
+    public Animator animator;
+    public SpriteRenderer sr;
+    public Tilemap tilemap;
+    public Tile crackedTile;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,11 +24,34 @@ public class Knight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += (Vector3)movement * speed * Time.deltaTime;
+        animator.SetFloat("movement", movement.magnitude);
+        if (movement.x < 0)
+        {
+            sr.flipX = true;
+        }
+        if (movement.x > 0)
+        {
+            sr.flipX = false;
+        }
     }
 
     public void Footstep()
     {
-        SFX.Play();
+        Vector3Int cellPos = tilemap.WorldToCell(transform.position);
+        TileBase tileStepped = tilemap.GetTile(cellPos);
+
+        //SFX.Play();
+        impulseSource.GenerateImpulse();
+
+        if (tileStepped.name == "TX Tileset Grass Pavement 0")
+        {
+            tilemap.SetTile(cellPos, crackedTile);
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
     }
 }
