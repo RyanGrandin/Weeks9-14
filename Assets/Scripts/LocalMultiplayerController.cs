d51ac3e2cf864c26dfe637b2a8ccc7ffd57ea324
Ryan Grandin <rmgrandin8@gmail.com>
2026-04-08 10:10:40 -0400

    Got local multiplayer working.

---
+++ Assets/Scripts/LocalMultiplayerController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LocalMultiplayerController : MonoBehaviour
+{
+    public Vector2 movementInput;
+    public float speed = 5;
+    public PlayerInput playerInput;
+    public LocalMultiplayerManager manager;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (Vector3)movementInput * speed * Time.deltaTime;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("Player " + playerInput.playerIndex + " attacked.");
+            manager.PlayerAttacking(playerInput);
+        }
+    }
+}
