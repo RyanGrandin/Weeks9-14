30e0191327cb9e8c67d8a7b3b4ec283127292fd1
Ryan Grandin <rmgrandin8@gmail.com>
2026-04-08 22:47:00 -0400

    Added ink changing and pen follow.

--- Assets/Scripts/BookScript.cs
+++ Assets/Scripts/BookScript.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class BookScript : MonoBehaviour
 {
     Vector2 mousePos;
-    public SpriteRenderer drawingSR;
-    public List<SpriteRenderer> pg1Panels;
+    public List<SpriteRenderer> drawings;
+    public Image penIMG;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for (int i = 0; i < pg1Panels.Count; i++)
+        // sets all of the drawings' alphas to 0 when the game starts
+        for (int i = 0; i < drawings.Count; i++)
         {
-            pg1Panels[i].color = new Color(pg1Panels[i].color.r, pg1Panels[i].color.g, pg1Panels[i].color.b, 0);
+            drawings[i].color = new Color(drawings[i].color.r, drawings[i].color.g, drawings[i].color.b, 0);
         }
     }
 
@@ -27,25 +29,31 @@ public class BookScript : MonoBehaviour
     {
         if (context.performed)
         {
-            Debug.Log("Click!");
             mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
 
-            if (drawingSR.bounds.Contains(mousePos))
+            // checks drawings to see if the mouse is over any of their sprites' bounds
+            for (int i = 0; i < drawings.Count; i++)
             {
-                Debug.Log("Click in bound!");
-                StartCoroutine(IncreaseAlpha());
+                if (drawings[i].sprite.bounds.Contains(mousePos))
+                {
+                    drawings[i].color = new Color(penIMG.color.r, penIMG.color.g, penIMG.color.b, drawings[i].color.a);
+                    StartCoroutine(IncreaseAlpha(drawings[i]));
+                }
             }
         }
     }
 
-    IEnumerator IncreaseAlpha()
+    IEnumerator IncreaseAlpha(SpriteRenderer drawingSR)
     {
         float a = drawingSR.color.a;
 
         while (a < 255)
         {
             a += Time.deltaTime;
+            
+            // sets drawing to a new colour with an updated alpha value
             drawingSR.color = new Color(drawingSR.color.r, drawingSR.color.g, drawingSR.color.b, a);
+
             yield return null;
         }
     }
--- Assets/Scripts/InkScript.cs
+++ Assets/Scripts/InkScript.cs
@@ -1,7 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class InkScript : MonoBehaviour
 {
+    public Image penIMG;
+    public RectTransform penRectTransform;
+    Vector2 newPensition = new Vector2();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +17,26 @@ public class InkScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        penRectTransform.position = newPensition;
+    }
+
+    public void BlackInquip()
+    {
+        penIMG.color = Color.black;
+    }
+
+    public void RedInquip()
+    {
+        penIMG.color = Color.red;
+    }
+
+    public void BlueInquip()
+    {
+        penIMG.color = Color.blue;
+    }
+
+    public void OnPoint(InputAction.CallbackContext context)
+    {
+        newPensition = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>());
     }
 }
